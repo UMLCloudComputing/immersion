e16cda2f030a439035f78a8daf5dc0f230aab363
Gurpranked <gurpreet.singh.rehal@outlook.com>
2024-11-06 18:13:15 -0500

    chore: Some minor edits to the init file (setup file)

--- init.py
+++ init.py
@@ -5,12 +5,20 @@ NAME = "X-Engage-Api-Key"
 KEY = "esk_test_7772a5488f56d5def2dc911f5e54e5b2"
 
 headers = {
+    "Name": NAME,
+    "Value": KEY
+}
+ROOT_URL = "https://engage-api.campuslabs.com/api/v3.0/"
 
-response = session.get(url)
+# Only URLS for GET requests
+URLS = {
+    "events/category": ROOT_URL + "events/category",
+    "events/events/": ROOT_URL + ""
+}
+
+response = requests.get(URLS['events/'], headers=headers)
 
 response_data = response.json()
-
-
 
 print(response_data['items'][1]['userId']['username'])
 print(response_data['items'][0]['userId']['swipeCardIdentifier'])
