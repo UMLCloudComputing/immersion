56e904cc889b6754e27b7efe6c3c3499e258b05d
Gurpranked <gurpreet.singh.rehal@outlook.com>
2024-11-07 20:53:47 -0500

    chore: Some more changes to org test

--- orgtest.py
+++ orgtest.py
@@ -26,8 +26,13 @@ response = requests.get('https://engage-api.campuslabs.com/api/v3.0/events/event
 
 
 response_dict = response.json()
-print(response_dict['items'][0]['description'])
+
 test_file = open('test.txt', 'w')
+test_file.write(str(response_dict['items'][0]['id']) + '\n')
 test_file.write("Name: " + response_dict['items'][0]['name'] + '\n')
 test_file.write("Description: " + response_dict['items'][0]['description']  + '\n')
+test_file.write("Starts on: " + response_dict['items'][0]['startsOn']  + '\n')
+test_file.write("Ends on: " + response_dict['items'][0]['endsOn']  + '\n')
+test_file.write("Location: " + response_dict['items'][0]['address']['name'] + '\n')
+
 test_file.close()
