aeab586984f073b589b365f22291442b03300016
cjcocokrisp <chriscoco1205@gmail.com>
2025-01-25 13:20:09 -0500

    feat: add constructs for dynamodb tables

--- immersion/immersion_stack.py
+++ immersion/immersion_stack.py
@@ -1,19 +1,33 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb
 )
 from constructs import Construct
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class ImmersionStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        serverTable = dynamodb.TableV2(
+            self, 
+            f'{os.getenv('APP_NAME')}ServerTable',
+            partition_key=dynamodb.Attribute(name='serverId', type=dynamodb.AttributeType.STRING)
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ImmersionQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        cacheTable = dynamodb.TableV2(
+            self,
+            f'{os.getenv('APP_NAME')}APICacheTable',
+            partition_key=dynamodb.Attribute(name='clubId', type=dynamodb.AttributeType.STRING)
+        )
+
+        eventTable = dynamodb.TableV2(
+            self,
+            f'{os.getenv('APP_NAME')}EventTable',
+            partition_key=dynamodb.Attribute(name='eventId', type=dynamodb.AttributeType.STRING)
+        )
