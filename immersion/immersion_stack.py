8f9741b0dabedd4c1b26dd0e52f53e6251b8adc3
cjcocokrisp <chriscoco1205@gmail.com>
2025-02-02 11:04:50 -0500

    feat: add bot program and docker image from previous work and start setting up ecs cluster

--- immersion/immersion_stack.py
+++ immersion/immersion_stack.py
@@ -1,7 +1,11 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_dynamodb as dynamodb
+    aws_autoscaling as autoscaling,
+    aws_dynamodb as dynamodb,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecs_patterns as ecsp
 )
 from constructs import Construct
 from dotenv import load_dotenv
@@ -14,20 +18,51 @@ class ImmersionStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Container Cluster Component Defintion
+        vpc = ec2.Vpc(
+            self,
+            f'{os.getenv('APP_NAME')}VPC',
+        )
+
+        cluster = ecs.Cluster(
+            self,
+            f'{os.getenv('APP_NAME')}ServiceCluster',
+            vpc=vpc
+        )
+
+        app_task_defintion = ecs.FargateTaskDefinition(
+            self,
+            f'{os.getenv('APP_NAME')}DiscordAppTaskDefinition',
+            memory_limit_mib=1024, # 1 GB
+            cpu=512, # 0.5 vCPU
+        )
+
+        # TODO: app_task_defintion.add_container()
+
+        app_service = ecs.FargateService(
+            self,
+            f'{os.getenv('APP_NAME')}DiscordAppService',
+            cluster=cluster,
+        )
+
+        # DynamoDB Table Definitions
         serverTable = dynamodb.TableV2(
             self, 
             f'{os.getenv('APP_NAME')}ServerTable',
-            partition_key=dynamodb.Attribute(name='serverId', type=dynamodb.AttributeType.STRING)
+            partition_key=dynamodb.Attribute(name='serverId', type=dynamodb.AttributeType.STRING),
+            deletion_protection=True
         )
 
         cacheTable = dynamodb.TableV2(
             self,
             f'{os.getenv('APP_NAME')}APICacheTable',
-            partition_key=dynamodb.Attribute(name='clubId', type=dynamodb.AttributeType.STRING)
+            partition_key=dynamodb.Attribute(name='clubId', type=dynamodb.AttributeType.STRING),
+            deletion_protection=True
         )
 
         eventTable = dynamodb.TableV2(
             self,
             f'{os.getenv('APP_NAME')}EventTable',
-            partition_key=dynamodb.Attribute(name='eventId', type=dynamodb.AttributeType.STRING)
+            partition_key=dynamodb.Attribute(name='eventId', type=dynamodb.AttributeType.STRING),
+            deletion_protection=True
         )
