9d44884a870df670289a102febe087fae056ae74
Gurpranked <gurpreet.singh.rehal@outlook.com>
2025-03-27 21:02:57 -0400

    chore: Init for organization data pull lambda function

--- src/data_filters/club_information/index.py
+++ src/data_filters/club_information/index.py
@@ -1,14 +1,56 @@
 import boto3
 import json
+import requests
 import os
 
-def lambda_handler(event, context):
-    sqs = boto3.client('sqs')
-    testMessage = {
-        'message': 'hello from lambda'
+# Parse a single organization to extract only important info
+def parse_info(item):
+    data = {
+        "OrgId": item['id'],
+        "Name": item['name'],
+        "PrimaryContact": item['PrimaryContactId']['campusEmail'],
+        "ImageUrl": f"https://se-images.campuslabs.com/clink/images/{item['profilePicture']}",
+        "WebsiteKey": f"https://umasslowellclubs.campuslabs.com/engage/organization/{item['websiteKey']}"
     }
 
-    sqs.send_message(
-        QueueUrl=os.getenv('QUEUE_URL'),
-        MessageBody=json.dumps(testMessage)
+    return data
+
+def lambda_handler(event, context):
+    # Constants
+    api_endpoint = "https://engage-api.campuslabs.com/api/v3.0/organizations/organization?includeCategories=false&includeContactInfo=true&includeSocialMedia=false&isAdminOnly=false&isBranch=false&isShownInPublicDirectory=true"
+
+    # Service configuration
+    sqs = boto3.client('sqs')
+    ssm_client = boto3.client('ssm')
+    response = ssm_client.get_parameter(
+        Name="engage_api_key_test",
+        WithDecryption=True
     )
+
+    # Define headers with API key
+    api_key = response['Parameter']['Value']
+    headers = {
+        'Authorization': f'Bearer {api_key}',
+        'Content-Type': 'application/json'
+    }
+
+    try:
+        response = requests.get(api_endpoint, headers=headers)
+
+        # Process response from API
+        data = response.json()
+        parsed_data = []
+        for item in data['items']:
+            parsed_data.append(parse_info(item))
+        
+        return_data = {
+            "Header": "club_information",
+            "Body": json.dumps(parsed_data)
+        }
+        sqs.send_message(
+            QueueUrl=os.getenv('QUEUE_URL'),
+            MessageBody=json.dumps(return_data)
+        )
+    except requests.exceptions.RequestException as e:
+        print(f"Error making API call: {e}")
+    
