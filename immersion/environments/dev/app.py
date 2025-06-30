0df217d3bfe1ee4682ded7a584b67caeef2b3e5b
HoustonBoston <myhorsefly12345@gmail.com>
2025-06-30 09:17:48 -0700

    Hopefully this provides the right context for AWS CDK deploy commands

--- immersion/environments/dev/app.py
+++ immersion/environments/dev/app.py
@@ -2,6 +2,11 @@ from immersion_stack import ImmersionStack
 import aws_cdk as cdk
 
 app = cdk.App()
-ImmersionStack(app, "ImmersionStackDev")
+ImmersionStack(app, "ImmersionStackDev", {
+    "env": {
+        "account": "default",
+        "region": "us-east-1"
+    }
+})
 
 app.synth()
