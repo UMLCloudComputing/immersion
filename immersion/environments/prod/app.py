e9114f87e3a082aa61f18d349f9cbf69e081f797
HoustonBoston <myhorsefly12345@gmail.com>
2025-06-30 09:49:56 -0700

    Added aws account id as a secret to gh actions

--- immersion/environments/prod/app.py
+++ immersion/environments/prod/app.py
@@ -1,7 +1,13 @@
 from immersion_stack import ImmersionStack
 import aws_cdk as cdk
+import os
 
 app = cdk.App()
-ImmersionStack(app, "ImmersionStackProd")
+ImmersionStack(app, "ImmersionStackDev",
+    env = {
+        "account": os.getenv('CICD_ACCOUNT_ID'), # Assuming it's only being deployed with GH Actions
+        "region": "us-east-1"
+    }
+)
 
 app.synth()
